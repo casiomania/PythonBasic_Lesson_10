226f23d269eaa2928b12ea1ca1c12bf4190028f0
Serhii <casiomania@proton.me>
2024-03-12 22:44:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# # Lesson 10 / Homework
+# Write validations using regular expressions and test them on different cases:
+# - Home phone number (only digits and number length)
+# - Mobile phone number (only digits, possible plus sign, number length)
+# - email (presence of @, domain: gmail.com, for example, minimum length and maximum length of your choice)
+# - Full name of the client (3 words, minimum length 2 characters, maximum length 20)
+# optional:
+# - Password (minimum: one uppercase letter, one lowercase letter, one digit, one symbol, password length - from 8 to 16 characters)
+
